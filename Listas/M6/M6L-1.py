e2e72aaff354faa13b7bb2773420fcf8ba9fa852
DevCezarIs <cezar.dev24@gmail.com>
2024-04-24 20:45:27 -0300

    M6: update new resolution
    Created the resolution for the first question on the sixth list.

---
+++ Listas/M6/M6L-1.py
@@ -0,0 +1,20 @@
+'''Faça um programa que leia uma frase, e duas letras quaisquer do usuário. A seguir,
+troque na frase todas as ocorrências da primeira letra fornecida pela segunda e
+imprima a nova frase'''
+
+while True:
+    phase = input("Write any sentence here: ")
+    letter_1 = input("Write the letter in the sentence to be replaced: ")
+    letter_2 = input("Write the letter in the sentence that will replace it: ")
+
+    if letter_1 and letter_2 not in phase:
+        print(f"The typed letter is not in the sentence, it remains the same")    
+    else:
+        phase_replaced = phase.replace(letter_1, letter_2)
+        print(f"Agora sua Frase é:\n {phase_replaced}")
+    
+
+    verify = input("Do you want continue? [Y/N]")
+
+    if verify.lower().strip() == y:
+        break
