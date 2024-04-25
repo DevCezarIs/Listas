39e71cb57399e072f15010d6015bec0e4e8086bd
DevCezarIs <cezar.dev24@gmail.com>
2024-04-24 21:03:59 -0300

    M6L-1: update translate
    Translated a sentence that was in Portuguese.

--- Listas/M6/M6L-1.py
+++ Listas/M6/M6L-1.py
@@ -6,15 +6,17 @@ while True:
     phase = input("Write any sentence here: ")
     letter_1 = input("Write the letter in the sentence to be replaced: ")
     letter_2 = input("Write the letter in the sentence that will replace it: ")
+    y = 0
+
 
     if letter_1 and letter_2 not in phase:
         print(f"The typed letter is not in the sentence, it remains the same")    
     else:
         phase_replaced = phase.replace(letter_1, letter_2)
-        print(f"Agora sua Frase é:\n {phase_replaced}")
+        print(f"Now your sentence is:\n {phase_replaced}")
     
 
+#Loop for make more sentences
     verify = input("Do you want continue? [Y/N]")
-
     if verify.lower().strip() == y:
         break
